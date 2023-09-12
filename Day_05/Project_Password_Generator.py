ffc912b755e1bdeb943e2e47966f7f88b4896301
Samuel Nowlan <samuel.nowlan@modulrfinance.com>
2023-09-12 21:58:31 +0100

    Day05_fix

--- Day_05/Project_Password_Generator.py
+++ Day_05/Project_Password_Generator.py
@@ -1,27 +1,41 @@
-# Password Generator Project
+#Password Generator Project
 import random
-
-# Lists containing characters for generating the password
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-# Introduction and user input for password customization
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input("How many symbols would you like?\n"))
-nr_numbers = int(input("How many numbers would you like?\n"))
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Generating specified number of letters, symbols, and numbers for the password
-list_of_letters = [random.choice(letters) for _ in range(nr_letters)]
-list_of_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-list_of_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
-# Merging lists and shuffling to create a randomized password
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+
+list_of_letters = []
+list_of_numbers = []
+list_of_symbols = []
+
+for n in range(0, nr_letters):
+    random_letter_index = random.randint(0, len(letters) -1)
+    list_of_letters += [letters[random_letter_index]]
+
+for n in range(0, nr_symbols):
+    random_symbol_index = random.randint(0, len(symbols) -1)
+    list_of_symbols += [symbols[random_symbol_index]]
+
+for n in range(0, nr_numbers):
+    random_number_index = random.randint(0, len(numbers) -1)
+    list_of_numbers += [numbers[random_number_index]]
+
 password_list = list_of_letters + list_of_symbols + list_of_numbers
 random.shuffle(password_list)
 
-# Converting the list into a string
 password = ''.join(password_list)
 
 print(password)
